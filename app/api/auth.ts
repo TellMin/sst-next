e6e53f3f17976e63b237d5cec9d8c69b3d5f221e
TellMin <dhgirkcls@gmail.com>
2023-07-06 20:09:17 +0900

    Write user data to ddb

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -1,5 +1,8 @@
 import { AuthHandler, GoogleAdapter, Session } from "sst/node/auth";
 import { Config } from "sst/node/config";
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
+import { Table } from "sst/node/table";
 
 export const handler = AuthHandler({
   providers: {
@@ -8,6 +11,20 @@ export const handler = AuthHandler({
       clientID: Config.GOOGLE_CLIENT_ID,
       onSuccess: async (tokenset) => {
         const claims = tokenset.claims();
+
+        const ddb = new DynamoDBClient({});
+        await ddb.send(
+          new PutItemCommand({
+            TableName: Table.users.tableName,
+            Item: marshall({
+              userId: claims.sub,
+              email: claims.email,
+              picture: claims.picture,
+              name: claims.given_name,
+            }),
+          })
+        );
+
         return Session.parameter({
           redirect: "http://localhost:3000",
           type: "user",
